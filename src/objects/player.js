a6200bd6af97644ce8a05e47be2b50efe3c574c8
Travis Chase <gaudeon@cpan.org>
2017-05-17 11:34:35 -0600

    fixing to reference maps

--- src/objects/player.js
+++ src/objects/player.js
@@ -104,7 +104,7 @@ App.Player = (function () {
         if (!playerMoving) {
             this.animations.stop();
         }
-        this.game.physics.arcade.collide(this, this.game.global.forest.layers.Collisions);
+        this.game.physics.arcade.collide(this, this.game.global.maps.layers.Collisions);
         var g = this.game.global;
         var p = g.player.body;
         if (g.player.playerName && (g.vx != p.velocity.x || g.vy != p.velocity.y)) {
